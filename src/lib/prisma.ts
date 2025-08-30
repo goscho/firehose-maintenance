b5ddb564a8b2d5ccc51f1009e3f905c38da83460
Ingo Schock <code@ingoschock.de>
2025-08-30 14:42:27 +0200

    feat: configure query logging for Prisma client

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,10 +1,41 @@
-import { PrismaClient } from "@/app/generated/prisma";
+import { Prisma, PrismaClient } from "@/app/generated/prisma";
 
 const globalForPrisma = global as unknown as {
   prisma: PrismaClient;
 };
 
-const prisma = globalForPrisma.prisma || new PrismaClient();
+const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: [
+      {
+        emit: "event",
+        level: "query",
+      },
+      {
+        emit: "stdout",
+        level: "error",
+      },
+      {
+        emit: "stdout",
+        level: "info",
+      },
+      {
+        emit: "stdout",
+        level: "warn",
+      },
+    ],
+  });
+
+// prevent multiple handlers during development on hot reloads
+if (!globalForPrisma.prisma) {
+  // @ts-expect-error - PrismaClient is not typed correctly
+  prisma.$on("query", (e: Prisma.QueryEvent) => {
+    console.log("Query: " + e.query);
+    console.log("Params: " + e.params);
+    console.log("Duration: " + e.duration + "ms");
+  });
+}
 
 // prevent multiple connections during development on hot reloads
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
