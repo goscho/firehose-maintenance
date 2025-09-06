853d7337c3748e012939c4fe846c3a663ccb3e6b
Ingo Schock <code@ingoschock.de>
2025-09-06 23:48:07 +0200

    feat: add basic error handling on NewHosePage

--- src/app/hose/new/page.tsx
+++ src/app/hose/new/page.tsx
@@ -4,24 +4,28 @@ import { useRouter } from "next/navigation";
 import NewHoseForm from "@/app/_components/new-hose-form";
 import { FireHose } from "@/lib/types";
 import { createFireHose } from "@/lib/fireHoseRepository";
+import { useState } from "react";
 
 export default function NewHosePage() {
   const router = useRouter();
+  const [error, setError] = useState("");
 
   // Handle form submission
-  const handleSubmit = (data: FireHose) => {
+  const handleSubmit = async (data: FireHose) => {
     console.log("Submitting new hose:", data);
 
-    createFireHose({
-      number: data.number!,
-      ownerId: data.owner!.id,
-      length: data.length!,
-      diameter: data.diameter!,
-      commissionedAt: data.commissionedAt!,
-    });
-
-    // For now, just navigate back to the home page
-    router.push(`/hose/${data.owner!.marker}__${data.number}`);
+    try {
+      await createFireHose({
+        number: data.number!,
+        ownerId: data.owner!.id,
+        length: data.length!,
+        diameter: data.diameter!,
+        commissionedAt: data.commissionedAt!,
+      });
+      router.push(`/hose/${data.owner!.marker}__${data.number}`);
+    } catch (err) {
+      setError("Fehler beim Erstellen des Schlauchs. " + err);
+    }
   };
 
   // Handle form cancellation
@@ -33,7 +37,7 @@ export default function NewHosePage() {
   return (
     <main className="flex min-h-screen flex-col items-center p-6 gap-6">
       <h1 className="text-3xl font-bold">Neuen Schlauch anlegen</h1>
-
+      {error && <p className="text-red-500">{error}</p>}
       <NewHoseForm onSubmit={handleSubmit} onCancel={handleCancel} />
     </main>
   );
