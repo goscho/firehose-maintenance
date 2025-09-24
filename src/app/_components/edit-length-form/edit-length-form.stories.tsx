b89072512b1bb470417b5c1c90d6627d3eec6bfd
Ingo Schock <code@ingoschock.de>
2025-09-24 14:19:07 +0200

    feat: implement EditLengthForm component

---
+++ src/app/_components/edit-length-form/edit-length-form.stories.tsx
@@ -0,0 +1,36 @@
+import type { Meta, StoryObj } from "@storybook/nextjs";
+import { fn } from "storybook/test";
+import EditLengthForm from "./edit-length-form";
+
+const meta: Meta<typeof EditLengthForm> = {
+  component: EditLengthForm,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    currentLength: { control: "number" },
+  },
+  args: { onSave: fn(), onCancel: fn() },
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    currentLength: 20,
+  },
+};
+
+export const LongHose: Story = {
+  args: {
+    currentLength: 100,
+  },
+};
+
+export const ShortHose: Story = {
+  args: {
+    currentLength: 5,
+  },
+};
