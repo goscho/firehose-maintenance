5a1c69497f8236d70450737f1770f0d482f2087d
Ingo Schock <code@ingoschock.de>
2025-12-26 22:28:04 +0100

    feat: integrate Sonner for toast notifications

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import PageHeader from "@/app/_components/page-header";
+import { Toaster } from "sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,17 @@ export default function RootLayout({
           <PageHeader />
           <main>{children}</main>
         </div>
+        <Toaster
+          position={"top-right"}
+          richColors={true}
+          theme={"light"}
+          toastOptions={{
+            classNames: {
+              title: "!text-xl",
+              description: "!text-base",
+            },
+          }}
+        />
       </body>
     </html>
   );
